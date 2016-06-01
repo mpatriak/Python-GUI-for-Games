5d7721e63316b1f20fb9fa25358a4c6e974e7e39
Michal Patriak <mpatriak15@outlook.com>
2016-06-01 05:50:02 -0700

    Basic GUI is operational

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ import rockpaperscissors
 
 # define a window the game will be viewed in
 root = Tk()
-root.title ("Mike Patriak's MiniGame Collection")
+root.title ("Michal Patriak's MiniGame Collection")
 
 # create a frame within the window
 mainframe = Frame(root, height = 200, width = 500)
@@ -16,3 +16,20 @@ mainframe.pack_propagate(0)
 # pad the borders so content won't touch sides
 mainframe.pack(padx = 5, pady = 5)
 
+# define an intro to display in the window
+intro = Label(mainframe, text = """Welcome to Michal Patriak's MiniGame
+                                Collection. Please select one of the
+                                following games to play: """)
+# display the intro
+intro.pack(side = TOP)
+
+# create a button for rockpaperscissors
+rps_button = Button(mainframe, text = "Rock, Paper, Scissors",
+                    command = rockpaperscissors.gui)
+rps_button.pack()
+
+# exit button
+exit_button = Button(mainframe, text = "Quit", command = root.destroy)
+exit_button.pack(side = BOTTOM)
+
+root.mainloop()
